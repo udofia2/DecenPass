42b5dc358649a1af1069f487b123152440ae68be
Joel Salasi <tanunajoel0@gmail.com>
2024-11-05 20:56:16 +0100

    fixings

--- projects/decent_pass-frontend/src/dashboard/routes/index.ts
+++ projects/decent_pass-frontend/src/dashboard/routes/index.ts
@@ -5,8 +5,7 @@ const FormLayout = lazy(() => import('../Form/FormLayout'));
 const Profile = lazy(() => import('../Profile'));
 const RegisterUser = lazy(() => import('../RegisterUser'));
 const Settings = lazy(() => import('../Settings'));
-const Tables = lazy(() => import('../Tables'));
-
+ 
 
 const coreRoutes = [
 
@@ -15,21 +14,7 @@ const coreRoutes = [
     title: 'Profile',
     component: Profile,
   },
-  {
-    path: '/dashboard/forms/form-elements',
-    title: 'Forms Elements',
-    component: FormElements,
-  },
-  {
-    path: '/dashboard/forms/form-layout',
-    title: 'Form Layouts',
-    component: FormLayout,
-  },
-  {
-    path: '/dashboard/tables',
-    title: 'Tables',
-    component: Tables,
-  },
+    
   {
     path: '/dashboard/settings',
     title: 'Settings',
